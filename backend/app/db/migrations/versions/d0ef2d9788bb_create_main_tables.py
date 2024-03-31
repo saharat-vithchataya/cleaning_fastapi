307d6ca09f60c185153d7559a71a35ce7ceee989
Saharat Vithchataya <vithchataya.saharat@gmail.com>
2024-03-31 17:33:06 +0700

    create users endpoint

--- backend/app/db/migrations/versions/d0ef2d9788bb_create_main_tables.py
+++ backend/app/db/migrations/versions/d0ef2d9788bb_create_main_tables.py
@@ -5,31 +5,107 @@ Revises:
 Create Date: 2024-03-31 07:39:28.422401
  
 """
+
 from alembic import op
 import sqlalchemy as sa
+from typing import Tuple
 
- 
+
 # revision identifiers, used by Alembic
-revision = 'd0ef2d9788bb'
+revision = "d0ef2d9788bb"
 down_revision = None
 branch_labels = None
 depends_on = None
- 
- 
+
+
+def create_updated_at_trigger() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS
+        $$
+        BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
+        """
+    )
+
+
+def timestamps(indexed: bool = False) -> Tuple[sa.Column, sa.Column]:
+    return (
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+    )
+
+
 def create_cleanings_table() -> None:
     op.create_table(
         "cleanings",
         sa.Column("id", sa.Integer, primary_key=True),
         sa.Column("name", sa.Text, nullable=False, index=True),
         sa.Column("description", sa.Text, nullable=True),
-        sa.Column("cleaning_type", sa.Text, nullable=False, server_default="spot_clean"),
+        sa.Column(
+            "cleaning_type", sa.Text, nullable=False, server_default="spot_clean"
+        ),
         sa.Column("price", sa.Numeric(10, 2), nullable=False),
+        *timestamps(),
     )
- 
- 
+    op.execute(
+        """
+        CREATE TRIGGER update_cleanings_modtime
+            BEFORE UPDATE
+            ON cleanings
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
+
+
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email_verified", sa.Boolean, nullable=False, server_default="False"),
+        sa.Column("salt", sa.Text, nullable=False),
+        sa.Column("password", sa.Text, nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default="True"),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_user_modtime
+            BEFORE UPDATE
+            ON users
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
+
+
 def upgrade() -> None:
+    create_updated_at_trigger()
     create_cleanings_table()
- 
- 
+    create_users_table()
+
+
 def downgrade() -> None:
+    op.drop_table("users")
     op.drop_table("cleanings")
+    op.execute("DROP FUNCTION update_updated_at_column")
--- backend/app/models/core.py
+++ backend/app/models/core.py
@@ -1,7 +1,20 @@
-from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, validator
+
 
 class CoreModel(BaseModel):
     pass
+
+
+class DateTimeModelMixin(BaseModel):
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    @validator("created_at", "updated_at", pre=True)
+    def default_datetime(cls, value: datetime) -> datetime:
+        return value or datetime.datetime.now()
+
 
 class IDModelMixin(BaseModel):
     id: int
